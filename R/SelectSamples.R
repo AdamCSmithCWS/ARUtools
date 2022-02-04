3d2a0585487827f36a003e1c38478a9bb66b2158
David Hope <david.hope@ec.gc.ca>
2022-02-04 11:59:04 -0500

    Added ability to have stratum specific N

--- R/SelectSamples.R
+++ R/SelectSamples.R
@@ -62,11 +62,14 @@ fun_aru_samp <- function(df, N, os, seed, strat_, selprob_id, x, y, ...) {
   list2env(list(...), envir = environment())
 
   print(c(mindis, maxtry, DesignID))
-
+  if(length(N)==1){
   Stratdsgn <- rep(N, length(arus))
-
   n_os <-  round(N*os)
   names(Stratdsgn) <- arus
+  } else if ( all(arus %in% names(N)) ){
+    Stratdsgn <- N
+    n_os <- lapply(FUN = function(x) x * os, X = N )
+  } else {simpleError("N should either be single value or list with all strata ID. Not all Strata found in N and N has length >1")}
 
   if(n_os==0) n_os <- NULL
 
